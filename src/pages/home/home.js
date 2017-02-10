99b9c9d7416040d3582eccc70e93063f6d438c05
lany9527 <7259lanyang>
2017-02-10 17:42:47 +0800

    超时 断线重连

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,6 +3,8 @@ import immutable from 'seamless-immutable';
 //import { connect } from 'labrador-redux';
 // import api from '../../utils/api'
 import Ws from '../../utils/resource'
+import WxResource from '../../utils/wx-resource'
+// import WxResource from 'wx-resource'
 const {any} = PropTypes;
 
 class Home extends Component {
@@ -27,30 +29,30 @@ class Home extends Component {
   }
 
   onLoad() {
-    let token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0ODY3MTAwMjIsImxldmVsIjoiIiwidWlkIjoiZTE3MmQ0NGUtZGY5Ni00NzBjLTlmM2QtMWJkN2RlNjU3MTA0In0.Dq5vp_eeVay25YaNNzLjRQsjqghRvfScvpntTT6wTQj1klZI3se3qErpXNn6isDg5PyWp6AENlZ3o-l-IeicTwd4BndMOCrUhHTz769F_--3cqDGxfMir4e9f1lfWWvI4RpD4GxLc38fFYaSwVx2xe5UzHkJbxMnsN21sdh6bvU";
+    let token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0ODY3OTY1MzIsImxldmVsIjoiIiwidWlkIjoiZTE3MmQ0NGUtZGY5Ni00NzBjLTlmM2QtMWJkN2RlNjU3MTA0In0.bQNfYVBZWaLSzMPo9z_aIRfrMuu_D_1LWFDql1cIYDXZ6Y7q_rbNUwaYoOiR4-Gkx9rJM_hAjl3s3LB8tvzeHH8vzDIfPdf0yo1_Iv7KhBI8P7Kgkya6FzefqKpwf1bsyj83Gf1M_J7AGGmuZ3ssFPQJzt77oKRI70MjQGINA6c";
     var data = {};
 
-    const ws = new Ws("ws://192.168.8.138/api/ws");
-    ws.get(
-      {
-        url: 'http://192.168.8.138/api/v1/user/auth/status'
-      },
-      token
-    ).then(function (res) {
-      console.log(res);
-      data = res;
-    });
-
-    // ws.post({
-    //   url: 'http://192.168.8.138/api/v1/user/auth/login',
-    //   data: {
-    //     "username": "826781877142",
-    //     "password": "111111"
-    //   }
-    // }).then(function (res) {
+    const wxResource = new WxResource("ws://192.168.8.138/api/ws");
+    // wxResource.get(
+    //   {
+    //     url: 'http://192.168.8.138/api/v1/user/auth/status'
+    //   },
+    //   token
+    // ).then(function (res) {
     //   console.log(res);
-    //   title = res;
+    //   data = res;
     // });
+
+    wxResource.post({
+      url: 'http://192.168.8.138/api/v1/user/auth/login',
+      data: {
+        "username": "826781877142",
+        "password": "111111"
+      }
+    }).then(function (res) {
+      console.log(res);
+      title = res;
+    });
     console.log("promise获取到的数据：",data);
     this.setState({ title: "14866340916919g851j" });
   }
---
+++ src/utils/wx-resource.ts
@@ -0,0 +1,172 @@
+/**
+ * Created by littlestone on 2017/2/10.
+ */
+
+import {Promise} from 'es6-promise';
+declare const wx: any;
+
+interface ReqObj {
+  url: string;
+  data: any;
+}
+class WxResource {
+  retryTime: number = 1000; //断线重连时间间隔
+  constructor(private wsUrl: string, private reqObj: ReqObj) {
+    this.connect();
+  }
+
+  /**
+   * 连接webSocket
+   * @returns {WxResource}
+   */
+  public connect(): WxResource {
+    let _that = this;
+    setTimeout(function () {
+      wx.connectSocket({
+        url: _that.wsUrl
+      });
+    },200000);
+
+    return this;
+  }
+
+  /**
+   * get 方法
+   * @param reqObj  传入的请求对象
+   * @param token   传入的token【非必传参数】
+   * @returns {Promise}
+   */
+  public get(reqObj: ReqObj, token?: string): Promise<any> {
+    let _that = this;
+    return new Promise((resolve, reject) => {
+      wx.onSocketOpen(function (res) {
+        console.log('WebSocket connection has been opened!', res);
+        _that.sendMsg(reqObj, "GET", token);
+      });
+      this.receiveMsg(resolve);
+      this.handleError(reject);
+      this.handleSocketClose()
+    })
+  }
+
+  /**
+   * post 方法
+   * @param reqObj  传入的请求对象
+   * @param token   传入的token【非必传参数】
+   * @returns {Promise}
+   */
+  public post(reqObj: ReqObj, token?: string): Promise<any> {
+    let _that = this;
+    return new Promise((resolve, reject) => {
+      wx.onSocketOpen(function (res) {
+        console.log('WebSocket connection has been opened!', res);
+        _that.sendMsg(reqObj, "POST");
+      });
+      this.receiveMsg(resolve);
+
+      this.handleError(reject);
+    })
+  }
+
+  /**
+   * delete 方法
+   * @param reqObj   传入的请求对象
+   * @param token    传入的token【非必传参数】
+   * @returns {Promise}
+   */
+  public delete(reqObj: ReqObj, token?: string): Promise<any> {
+    let _that = this;
+    return new Promise((resolve, reject) => {
+      wx.onSocketOpen(function (res) {
+        console.log('WebSocket connection has been opened!', res);
+        _that.sendMsg(reqObj, "DELETE");
+      });
+      this.receiveMsg(resolve);
+
+      this.handleError(reject);
+    })
+  }
+
+  /**
+   * update 方法
+   * @param reqObj   传入的请求对象
+   * @param token    传入的token【非必传参数】
+   * @returns {Promise}
+   */
+  public update(reqObj: ReqObj, token?: string): Promise<any> {
+    let _that = this;
+    return new Promise((resolve, reject) => {
+      wx.onSocketOpen(function (res) {
+        console.log('WebSocket connection has been opened!', res);
+        _that.sendMsg(reqObj, "UPDATE");
+      });
+      this.receiveMsg(resolve);
+
+      this.handleError(reject);
+    })
+  }
+
+  /**
+   * 处理webSocket发送的信息
+   * @param reqObj
+   * @param method  请求方法 GET  POST ...
+   * @param token
+   */
+  private sendMsg(reqObj: ReqObj, method: string, token?: string) {
+
+    // 判断是否传入token
+    let header = {};
+    if (token === undefined) {
+      console.log("no token");
+      header = {
+        "S-Request-Id": Date.now() + Math.random().toString(20).substr(2, 6)
+      }
+    } else if (token !== undefined) {
+      console.log("get token");
+      header = {
+        "S-Request-Id": Date.now() + Math.random().toString(20).substr(2, 6),
+        "Authentication": "Bearer " + token
+      }
+    }
+    wx.sendSocketMessage({
+      data: JSON.stringify({
+        "method": method,
+        "url": reqObj.url,
+        "header": header,
+        "body": JSON.stringify(reqObj.data)
+      }),
+      success: function (res) {
+        console.log("发送成功", res)
+      },
+      fail: function (res) {
+        console.log("发送失败", res)
+      }
+    });
+  }
+
+  // 处理错误信息
+  handleError(reject) {
+    wx.onSocketError(function (res) {
+      reject(res);
+      console.log(res, 'WebSocket连接打开失败，请检查！')
+    });
+  }
+
+  // 处理服务器返回内容
+  receiveMsg(resolve) {
+    wx.onSocketMessage(function (res) {
+      resolve(JSON.parse(res.data));
+    });
+  }
+  handleSocketClose(){
+    wx.onSocketClose(function (res) {
+      console.log("socketClose, try to connecting ...", res);
+      setTimeout(function () {
+        this.connect();
+      },this.retryTime)
+    })
+  }
+
+}
+
+export default WxResource;
